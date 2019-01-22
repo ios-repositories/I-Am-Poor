665563339669b05b79b43185a4013377ae45f30f
Jenny Manrique <jennymanrique2411@gmail.com>
2019-01-21 20:56:36 -0800

    I am Poor App, using UIImageLabel, Labels, and ImageAssets 2019-21-01

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jenny Manrique on 2019-01-20.
+//  Copyright © 2019 Jenny Manrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
